7fd2cf7f9db7b262b361b7bd6e85a3dbaab55c54
Lukas <lukas.oliveira0571@gmail.com>
2022-12-20 13:23:13 -0300

    Colocando exemplos da utilização dos metodos

--- aprendendo-sintaxe-java/anatomia-classe/src/MinhaClasse.java
+++ aprendendo-sintaxe-java/anatomia-classe/src/MinhaClasse.java
@@ -1,8 +1,39 @@
-
 public class MinhaClasse{
 
     public static void main(String[] args) {
-        System.out.print("Ola mundo !");
+    // Teste de variáveis mutáveis e imutáveis
+        // int ano = 2022;
+        // final int ANO_NASCIMENTO_97 = 1997;
+        // int resultado = ano - ANO_NASCIMENTO_97 ;
+        // System.out.print(resultado);
+
+    // Chamando o metodo.
+        // Exemplo 1 Calculando a idade
+        // int anoNascimento = 1997;
+        // int CalculandoIdade = CalculandoIdade(anoNascimento);
+        // System.out.println(CalculandoIdade);
+        
+        // Exemplo 2 Concatenando o primeiro nome com o segundo nome
+        // String primeiroNome,segundoNome;
+        // primeiroNome = "Lukas";
+        // segundoNome = "De Oliveira Almeida";
+        // String nomeCompleto = NomeCompleto(primeiroNome, segundoNome);
+        // System.out.println(nomeCompleto);
+
+
     }
+
+    // Declarando variáveis e metodos.
+        // Exemplo 1
+        // public static int CalculandoIdade(int anoNascimento){
+        // int ano = 2022;
+        // int resultado = ano - anoNascimento;
+        // return resultado;
+        // }
+        
+        // Exemplo2
+//        public static String NomeCompleto(String primeiroNome,String segundoNome){
+//             return "Nome Completo:".concat(primeiroNome).concat(" ").concat(segundoNome);
+//         }
 
 }
