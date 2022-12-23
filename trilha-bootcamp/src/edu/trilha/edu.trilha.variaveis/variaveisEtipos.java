261ae7d2f4bab2b771b656e203ad39efa1572526
Lukas <lukas.oliveira0571@gmail.com>
2022-12-22 23:09:37 -0300

    Criação do diretório sobre variáveis em java

---
+++ trilha-bootcamp/src/edu/trilha/edu.trilha.variaveis/variaveisEtipos.java
@@ -0,0 +1,18 @@
+public class variaveisEtipos {
+    
+    public static void main(String[] args) {
+        // Tipos primitivos
+        // Classe String
+        String nome = "Lukas";
+        int idade = 25;
+        final double PI = 3.14;
+        double salario = 2500;
+        float temperatura = 32.69F;
+        System.out.println("Nome:".concat(" ")+nome);
+        System.out.println("Idade".concat(" ")+idade);
+        System.out.println("Valor de PI:".concat(" ")+PI);
+        System.out.println("Salário".concat(" ")+salario);
+        System.out.println("Temperatura:".concat(" ")+temperatura);
+    
+    }
+}
