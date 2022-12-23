c29a6dfbe488ab69090e2cc61f70b00aecae0d1f
Lukas <lukas.oliveira0571@gmail.com>
2022-12-23 20:57:59 -0300

    Diretório sobre variáveis em java

---
+++ trilha-bootcamp/src/edu/trilha/edu.operadores/operadores.java
@@ -0,0 +1,67 @@
+
+public class operadores {
+    public static void main(String[] args) {
+        // Criando exemplos com Operadores aritimeticos
+        // int idade = 2022-1997;
+        // double saldo = 1200;
+        // int creditoLimite = 500;
+        // int debitosAtivosMensal = 200;
+        // double calcParcelas = (200*12) / 12 ; 
+        // double representacao_DebitosMensal_Sobre_Salario = (debitosAtivosMensal/100)*(saldo/100);
+        // System.out.println("Idade:"+idade);
+        // System.out.println("Despesas fixas:"+debitosAtivosMensal);
+        // System.out.println("saldo:"+saldo);
+        // System.out.println("Limite:"+creditoLimite);
+        // System.out.println("Parcelas Fixas ativas:"+calcParcelas);
+        // System.out.println(representacao_DebitosMensal_Sobre_Salario+"%");
+        
+        // Opetadores unários (incrementais)
+        // int numero = 1;
+        // numero ++;
+        // System.out.print(numero);
+
+        //Decrementando
+        // int numero = 5;
+        // numero --;
+        // System.out.print(numero);
+
+        //Invertendo operadores boleanos (Mudar estado sem atribuir Faça para a variavel)
+        // boolean variavel = true;
+
+        // Valor padrão
+        // System.out.println(variavel);
+        // // Valor invertido
+        // System.out.print(!variavel);
+
+        //Operador Ternário : Forma resumida de escolher de forma resumida uma condição
+        // int a = 5;
+        // int b = 7;
+        // String resultado = a==b ? "São iguais":"São diferentes";
+        // System.out.println(resultado);
+
+        //Operadores relacionais: 
+        // Maior que > 
+        // Menor que < 
+        // Igual há == 
+        // Diferente de !=
+        // Menor ou igual <=
+        // Maior ou igual >=
+
+        // int primeiroNumero = 10;
+        // int segundoNumero = 5;
+        // boolean resultado_1 = primeiroNumero > segundoNumero ? true:false; 
+        // boolean resultado_2 = segundoNumero != primeiroNumero ? true:false;
+    
+
+        // System.out.println("O número "+primeiroNumero+" é maior que "+segundoNumero);
+        // System.out.println(resultado_1);
+        // System.out.println("O número "+segundoNumero+" é diferente de "+primeiroNumero+" ?");
+        // System.out.println(resultado_2);
+
+        // String nome = "lukas";
+        // String primeiroNome = new String("LUKAS");
+
+        // String resultado = nome.equals(primeiroNome) == primeiroNome.equals(nome) ? "Sim":"Não";
+        // System.out.println(resultado); 
+    }
+}
